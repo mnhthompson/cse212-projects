791ac3fb2374f444503fa595d13a8b27e9d4137b
Mnhthompson <mnhthompson@gmail.com>
2024-11-04 12:11:41 -0700

    taking Turns

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -43,6 +43,14 @@ public class TakingTurnsQueue
             if (person.Turns > 1)
             {
                 person.Turns -= 1;
+                            
+                 if(person.Turns != 0)
+                {
+                    _people.Enqueue(person);
+                }
+            }
+            else if(person.Turns <= 0)
+            {
                 _people.Enqueue(person);
             }
 
